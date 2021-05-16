2128424f6d6485558424152a7bc96d2ec10f7edd
raiden <peckwood@users.noreply.github.com>
2021-05-16 18:40:23 +0800

    remove 'n' that is not need

--- list-songs-that-dont-have-lyrics.ps1
+++ list-songs-that-dont-have-lyrics.ps1
@@ -2,4 +2,4 @@ $pth = "D:/nextcloud/audio/music_repository";
 Get-ChildItem -Path $pth -Recurse -Include "*.mp3" | % { Process { 
     If ( ! ( Test-Path "$((Split-Path $_.FullName -Resolve))\$($_.BaseName).lrc" ) ) 
     { $_.FullName };
-    }};n
+    }};
